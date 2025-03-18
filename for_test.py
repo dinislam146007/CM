b8396040a38cd54666b3871ca76d016055882a39
dinislam <guitarp146007@gmail.com>
2025-03-18 09:33:07 +0500

    fix

--- for_test.py
+++ for_test.py
@@ -8,24 +8,22 @@ app = FastAPI()
 model = Llama(model_path="model.gguf", n_ctx=2048)
 
 @app.post("/predict")
-def predict(data: dict):
-    df = pd.DataFrame(data["indicators"])
-    last_values = df.iloc[-1]
+def predict():
 
     # Формируем текст для нейросети
     prompt = f"""
     Анализируй данные и укажи торговый сигнал:
 
-    RSI: {last_values['rsi']}
-    EMA(21): {last_values['ema21']}
-    EMA(49): {last_values['ema49']}
-    PPO: {last_values['ppoT']} / {last_values['ppoB']}
-    PPO % Rank: {last_values['pctRankT']} / {last_values['pctRankB']}
-    VSA: {last_values['vsa_signal']}
-    PA Pattern: {data["price_action_pattern"]}
-    RSI Signal: {last_values['signal_rsi']}
-    CM_Laguerre PPO PR Mkt Tops/Bottoms: {data["finish"]}
-    Div/Conv Signal: {data["divergence_convergence_signal"] if data["divergence_convergence_signal"] else "None"}
+    RSI: 44.00039432998614  
+    EMA(21): 91.47339715086062  
+    EMA(49): 93.20346005535993  
+    PPO: 1.2274159208232323 / -1.2274159208232323  
+    PPO % Rank: 88.0 / -35.0  
+    VSA:  
+    PA Pattern: None  
+    RSI Signal: Hold  
+    CM_Laguerre PPO PR Mkt Tops/Bottoms: sale  
+    Div/Conv Signal: bearish_divergence  
 
     Если есть бычьи сигналы → "buy".  
     Если есть медвежьи сигналы → "sale".  
