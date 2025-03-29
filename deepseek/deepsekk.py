ac7586a21d164f63a09a4d6a866473e6735bc8d0
dinislam <guitarp146007@gmail.com>
2025-03-29 09:00:05 +0500

    fix

--- deepseek/deepsekk.py
+++ deepseek/deepsekk.py
@@ -146,7 +146,7 @@ async def analyze_trading_signals(df,
     old_news_combined = "\n".join(old_news_list)
 
     response = await client.chat.completions.create(
-        model="deepseek/deepseek-r1-distill-qwen-32b",
+        model="openai/gpt-4o-mini",
         response_format={"type": "json_object"},
         messages=[
             {"role": "system", "content": prompt},
